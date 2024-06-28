6b24a0beaede21b5378b3ac394394270a240cf0e
Niloy Kundu <106025982+niloy263@users.noreply.github.com>
2024-06-28 14:28:01 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// 1 //
+                // 1 //
 
 function calculateSum(a, b) {
     return a + b;
@@ -6,7 +6,8 @@ function calculateSum(a, b) {
 console.log(calculateSum(5, 4));
 
 
-// 2 //
+                // 2 //
+
 
 function isEven(n) {
     return n % 2 === 0 ;
@@ -14,42 +15,53 @@ function isEven(n) {
 console.log(isEven(4));
 console.log(isEven(5));
 
-// 3 //
+
+                // 3 //
+
 
 function findMax(arr) {
     return Math.max(...arr);
 }
-
 console.log(findMax([10,22,13,14,5]));
 
-// 4 //
+
+                // 4 //
+
 
 function reverseString(str) {
     return str.split('').reverse().join('');
 }
 console.log(reverseString("Niloy"));  
 
-// 5 //
+
+                // 5 //
+
 
 function filterOddNumbers(arr) {
     return arr.filter(num => num % 2 !== 0);
 }
 console.log(filterOddNumbers([1, 2, 3, 4, 5]));
 
-// 6 //
-let num;
+                // 6 //
+
+
 function sumArray(arr) {
     return arr.reduce((sum, num) => sum + num, 0);
 }
 console.log(sumArray([1, 2, 3, 4, 5]));
 
-// 7 //
+                // 7 //
+
+
 function sortArray(arr) {
     return arr.sort((a, b) => a - b);
 }
 console.log(sortArray([5, 3, 8, 1, 2])); 
 
-// 8 //
+
+                // 8 //
+
+
 function capitalizeFirstLetter(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
