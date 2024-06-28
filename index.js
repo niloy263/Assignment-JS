af0ccda02bb8c2b8f86b194c4b3fab398b652d95
niloy263 <niloykundu263@gmail.com>
2024-06-28 14:18:33 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+// 1 //
+
+function calculateSum(a, b) {
+    return a + b;
+}
+console.log(calculateSum(5, 4));
+
+
+// 2 //
+
+function isEven(n) {
+    return n % 2 === 0 ;
+}
+console.log(isEven(4));
+console.log(isEven(5));
+
+// 3 //
+
+function findMax(arr) {
+    return Math.max(...arr);
+}
+
+console.log(findMax([10,22,13,14,5]));
+
+// 4 //
+
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+console.log(reverseString("Niloy"));  
+
+// 5 //
+
+function filterOddNumbers(arr) {
+    return arr.filter(num => num % 2 !== 0);
+}
+console.log(filterOddNumbers([1, 2, 3, 4, 5]));
+
+// 6 //
+let num;
+function sumArray(arr) {
+    return arr.reduce((sum, num) => sum + num, 0);
+}
+console.log(sumArray([1, 2, 3, 4, 5]));
+
+// 7 //
+function sortArray(arr) {
+    return arr.sort((a, b) => a - b);
+}
+console.log(sortArray([5, 3, 8, 1, 2])); 
+
+// 8 //
+function capitalizeFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capitalizeFirstLetter("hello")); 
